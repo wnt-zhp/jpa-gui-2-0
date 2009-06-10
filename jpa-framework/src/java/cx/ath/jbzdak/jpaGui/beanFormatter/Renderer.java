2fd829189474f441b6770aef4593de610734feac
Jacek Bzdak <jb@lucy.(none)>
2009-06-10 22:58:30 +0200

    xxxx

--- jpa-framework/src/java/cx/ath/jbzdak/jpaGui/beanFormatter/Renderer.java
+++
@@ -1,6 +0,0 @@
-package cx.ath.jbzdak.jpaGui.beanFormatter;
-
-public interface Renderer {
-
-	String render(Object object);
-}
--- jpa-framework/src/java/cx/ath/jbzdak/jpaGui/ui/form/Validator.java
+++
@@ -1,8 +0,0 @@
-package cx.ath.jbzdak.jpaGui.ui.form;
-
-import java.util.List;
-
-public interface Validator{
-	@SuppressWarnings("unchecked")
-	Object validate(Object entity, List<? extends FormElement>elements);
-}
