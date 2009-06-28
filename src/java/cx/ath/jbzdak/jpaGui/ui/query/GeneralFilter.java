ee9d56e47eb28f227d392e7132c1034cd25c3878
Jacek Bzdak <jb@lucy.(none)>
2009-06-28 22:18:52 +0200

    Formatowanie ;)

--- src/java/cx/ath/jbzdak/jpaGui/ui/query/GeneralFilter.java
+++ src/java/cx/ath/jbzdak/jpaGui/ui/query/GeneralFilter.java
@@ -1,37 +1,37 @@
 package cx.ath.jbzdak.jpaGui.ui.query;
 
-import javax.swing.RowFilter;
+import javax.swing.*;
 
 public abstract  class GeneralFilter extends RowFilter<Object,Object> {
-        private int[] columns;
+   private int[] columns;
 
-        GeneralFilter(int[] columns) {
+   GeneralFilter(int[] columns) {
+      this.columns = columns;
+   }
 
-            this.columns = columns;
-        }
 
-        public boolean include(Entry<? extends Object,? extends Object> value){
-            int count = value.getValueCount();
-            if (columns.length > 0) {
-                for (int i = columns.length - 1; i >= 0; i--) {
-                    int index = columns[i];
-                    if (index < count) {
-                        if (include(value, index)) {
-                            return true;
-                        }
-                    }
-                }
+   public boolean include(Entry<? extends Object,? extends Object> value){
+      int count = value.getValueCount();
+      if (columns.length > 0) {
+         for (int i = columns.length - 1; i >= 0; i--) {
+            int index = columns[i];
+            if (index < count) {
+               if (include(value, index)) {
+                  return true;
+               }
             }
-            else {
-                while (--count >= 0) {
-                    if (include(value, count)) {
-                        return true;
-                    }
-                }
+         }
+      }
+      else {
+         while (--count >= 0) {
+            if (include(value, count)) {
+               return true;
             }
-            return false;
-        }
+         }
+      }
+      return false;
+   }
 
-        protected abstract boolean include(
-              Entry<? extends Object,? extends Object> value, int index);
-    }
+   protected abstract boolean include(
+           Entry<? extends Object,? extends Object> value, int index);
+}
