bc457a557f19fe45b3e1d8aa8fe1571ac9e06a21
Jacek Bzdak <jb@lucy.(none)>
2009-07-12 13:39:56 +0200

    Bug powodujący rekurencję wartości przy odpalaniu format i parse po kolei wiele razy

--- src/java/cx/ath/jbzdak/jpaGui/ui/formatted/MyFormattedTextField.java
+++ src/java/cx/ath/jbzdak/jpaGui/ui/formatted/MyFormattedTextField.java
@@ -92,10 +92,8 @@ public class MyFormattedTextField extends JTextField{
 		Object oldValue = this.value;
 		this.value = value;
 		firePropertyChange("value", oldValue, this.value);
-        setParseResults(null);
-		//if(StringUtils.isEmpty(getText())){
-			formatValue();
-		//}
+      setParseResults(null);
+	   formatValue();
 	}
 
 	public void setValueFromBean(Object value) {
@@ -137,10 +135,10 @@ public class MyFormattedTextField extends JTextField{
 		firePropertyChange("userEnteredText", oldText, this.userEnteredText);
 	}
 
-	void attemptParseText(){
+	public void attemptParseText(){
       parsingText = true;
 		try {
-			setValue(formatter.parseValue(getText()));
+			setValue(formatter.parseValue(getUserEnteredText()));
 			setValueCurrent(true);
 			setParseResults(null);
 			setBackground(UIManager.getColor("TextField.background"));
